8400ef1bc42fbb9e28ce4aa6e7b065e3dc9f6672
BriAlpha <2745031785@qq.com>
2023-11-15 23:12:28 +0800

    run at version 8.1.1 successfully

--- app/src/main/java/com/example/yzbkaka/things/MainActivity.java
+++ app/src/main/java/com/example/yzbkaka/things/MainActivity.java
@@ -3,13 +3,12 @@ package com.example.yzbkaka.things;
 
 import android.content.Intent;
 import android.os.Build;
+import android.os.Bundle;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
 
 import com.example.yzbkaka.things.History.HistoryActivity;
 import com.example.yzbkaka.things.Log.LogActivity;
