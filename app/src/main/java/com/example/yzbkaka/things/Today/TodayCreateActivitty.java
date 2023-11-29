1034d126a431cc5bfafacf213f7b2c7db1c51b2c
XiLin20 <127746619+XiLin20@users.noreply.github.com>
2023-11-29 14:12:48 +0800

    Update TodayCreateActivitty.java

--- app/src/main/java/com/example/yzbkaka/things/Today/TodayCreateActivitty.java
+++ app/src/main/java/com/example/yzbkaka/things/Today/TodayCreateActivitty.java
@@ -19,10 +19,10 @@ import static com.example.yzbkaka.things.MainActivity.todayCount;
 
 
 public class TodayCreateActivitty extends AppCompatActivity {
-    private EditText editText;  //写下的计划
-    private Button finish;  //完成后的按钮
-    private String write;  //输入的内容
-    private Calendar calendar;  //获得时间
+    private EditText editText;  //Plans written down
+    private Button finish;  //Finished button
+    private String write;  //Inputs
+    private Calendar calendar;  //Acquisition time
     Date date;
     Plan plan = new Plan();
 
@@ -33,21 +33,21 @@ public class TodayCreateActivitty extends AppCompatActivity {
         editText = (EditText)findViewById(R.id.edit_text);
         finish = (Button)findViewById(R.id.finish);
 
-        calendar = Calendar.getInstance();  //获得实例
-        calendar.setTimeZone(TimeZone.getTimeZone("GMT+8:00"));  //设置为中国区的时间
+        calendar = Calendar.getInstance();  //Getting instances
+        calendar.setTimeZone(TimeZone.getTimeZone("GMT+8:00"));  //Set to China time
 
         finish.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                write = String.valueOf(editText.getText());  //获得输入的内容
+                write = String.valueOf(editText.getText());  //Getting the input
                 if(!write.isEmpty()){
                         plan.setWritePlan(write);
-                        plan.setYear(String.valueOf(calendar.get(Calendar.YEAR)));  //设置写下的时间,年
-                        plan.setMonth(String.valueOf(calendar.get(Calendar.MONTH)+1));  //设置月份(系统中是从0开始的)
-                        plan.setDay(String.valueOf(calendar.get(Calendar.DATE)));  //设置日期
-                        date = new Date(calendar.get(Calendar.YEAR),calendar.get(Calendar.MONTH)+1,calendar.get(Calendar.DATE));  //设置plan中的date
+                        plan.setYear(String.valueOf(calendar.get(Calendar.YEAR)));  //Set the time and year of writing
+                        plan.setMonth(String.valueOf(calendar.get(Calendar.MONTH)+1));  //Set the month (starts from 0 in the system)
+                        plan.setDay(String.valueOf(calendar.get(Calendar.DATE)));  //Setting the date
+                        date = new Date(calendar.get(Calendar.YEAR),calendar.get(Calendar.MONTH)+1,calendar.get(Calendar.DATE));  //Setting the date in plan
                         plan.setCreateTime(date);
-                        todayCount++;  //today统计数量加1
+                        todayCount++;  //Add 1 to the number of today statistics
                         plan.save();
                 }
                 finish();
